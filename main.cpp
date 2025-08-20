18a2a604f5c992f3a3bf42b9c834ddbd12fe3cd1
HungLuuSB <hungluu344@gmai.com>
2025-08-20 12:13:37 +0700

    Changed from using split to ordinary cin for reading inputs.

--- main.cpp
+++ main.cpp
@@ -1,118 +1,98 @@
-#include <iostream>
-#include <sstream>
 #include <fstream>
-#include <string>
-#include <vector>
-#include <math.h>
 #include <iomanip>
+#include <iostream>
+#include <math.h>
+#include <string>
 
 using namespace std;
 
 const float pi = 3.141526535;
 
-vector<string> split(const string& input, char delimiter) {
-    vector<string> tokens;
-    stringstream ss(input);
-    string item;
-
-    while (getline(ss, item, delimiter)) {
-        tokens.push_back(item);
-    }
-    return tokens;
-}
-
-
 void writeToFile(int bc, int bg, float nd) {
-    ofstream outputFileStream("output.out");
-    outputFileStream << bc << " " <<  bg << " " << fixed << setprecision(3) << nd;
-    outputFileStream.close();
+  ofstream outputFileStream("output.out");
+  outputFileStream << bc << " " << bg << " " << fixed << setprecision(3) << nd;
+  outputFileStream.close();
 }
 
-float calculateNBC(int dc) {
-    return dc * dc * sqrt(dc);
-}
+float calculateNBC(int dc) { return dc * dc * sqrt(dc); }
 
-float calculateNBG(int dg) {
-    return (dg * dg * pi) / 3;
-}
+float calculateNBG(int dg) { return (dg * dg * pi) / 3; }
 
 int calculateLDBC(int dc) {
-    if (dc >= 5)
-        return 2;
-    return 1;
+  if (dc >= 5)
+    return 2;
+  return 1;
 }
 
 int calculateLDBG(int dg) {
-    if (dg >= 8)
-        return 2;
-    return 1;
+  if (dg >= 8)
+    return 2;
+  return 1;
 }
 
-void calculate(string& weather, int& n, int dc, int dg, int& ld, int& bc, int& bg, float& nd) {
-    float n_bc = calculateNBC(dc);
-    float n_bg = calculateNBG(dg);
-    int ld_bc = calculateLDBC(dc);
-    int ld_bg = calculateLDBG(dg);
-    if (weather == "Windy") {
-        while (nd >= n_bc && ld >= ld_bc) {
-            bc++;
-            ld -= ld_bc;
-            nd -= n_bc;
-        }
-        while (nd >= n_bg && ld >= ld_bg) {
-            bg++;
-            ld -= ld_bg;
-            nd -= n_bg;
-        }
-    } else if (weather == "Rainy") {
-        bool can_cook_bc = true, can_cook_bg = true;
-        while (can_cook_bc || can_cook_bg) {
-            if (nd >= n_bc && ld >= ld_bc) {
-                bc++;
-                ld -= ld_bc;
-                nd -= n_bc;
-            } else {
-                can_cook_bc = false;
-            }
+void calculate(string &weather, int &n, int dc, int dg, int &ld, int &bc,
+               int &bg, float &nd) {
+  float n_bc = calculateNBC(dc);
+  float n_bg = calculateNBG(dg);
+  int ld_bc = calculateLDBC(dc);
+  int ld_bg = calculateLDBG(dg);
 
-            if (nd >= n_bg && ld >= ld_bg) {
-                bg++;
-                ld -= ld_bg;
-                nd -= n_bg;
-            } else {
-                can_cook_bg = false;
-            }
-        }
+  cout << "Nep needed to cook 1 BC = " << n_bc << endl;
+  cout << "La dong needed to cook 1 BC = " << ld_bc << endl;
+  cout << "Nep needed to cook 1 BG = " << n_bg << endl;
+  cout << "La dong needed to cook 1 BG = " << ld_bg << endl;
+  if (weather == "Windy") {
+    while (nd >= n_bc && ld >= ld_bc) {
+      bc++;
+      ld -= ld_bc;
+      nd -= n_bc;
     }
+    while (nd >= n_bg && ld >= ld_bg) {
+      bg++;
+      ld -= ld_bg;
+      nd -= n_bg;
+    }
+  } else if (weather == "Rainy") {
+    bool can_cook_bc = true, can_cook_bg = true;
+    while (can_cook_bc || can_cook_bg) {
+      if (nd >= n_bc && ld >= ld_bc) {
+        bc++;
+        ld -= ld_bc;
+        nd -= n_bc;
+      } else {
+        can_cook_bc = false;
+      }
+
+      if (nd >= n_bg && ld >= ld_bg) {
+        bg++;
+        ld -= ld_bg;
+        nd -= n_bg;
+      } else {
+        can_cook_bg = false;
+      }
+    }
+  }
 }
 
 int main() {
-    string value;
+  fstream inputFileStream("input.inp");
+  int n = 0, dc = 0, dg = 0, ld = 0, bc = 0, bg = 0;
+  float nd = 0;
+  string w;
 
-    ifstream inputFileStream("input.inp");
-    getline(inputFileStream, value);
-    inputFileStream.close();
+  inputFileStream >> n >> dc >> dg >> ld >> w;
+  inputFileStream.close();
 
-    vector<string> values = split(value, ' ');
+  cout << n << " " << dc << " " << dg << " " << ld << " " << w << endl;
+  nd = n;
 
-    int n = 0, dg = 0, dc = 0, ld = 0, bc = 0, bg = 0;
-    float nd = 0;
-    string w;
-
-    n = stoi(values[0]);
-    dc = stoi(values[1]);
-    dg = stoi(values[2]);
-    ld = stoi(values[3]);
-    w = values[4];
-
-    nd = n;
-
-    if (dc == 0 && dg == 0) {
-        writeToFile(-1, -1, n);
-        return 0;
-    }
-
-    calculate(w, n, dc, dg, ld, bc, bg, nd);
-    writeToFile(bc, bg, nd);
+  if (dc == 0 && dg == 0) {
+    writeToFile(-1, -1, n);
     return 0;
+  }
+
+  calculate(w, n, dc, dg, ld, bc, bg, nd);
+  writeToFile(bc, bg, nd);
+  cout << bc << " " << bg << " " << nd << endl;
+  return 0;
 }
